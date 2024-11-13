fdb1e99ff4200ba64525469f3b5dde0d5719a193
Juho Turunen <juho.turunen89@gmail.com>
2024-11-14 00:58:53 +0200

    Refactored db related files and added instructions

---
+++ src/entities/refrence.py
@@ -0,0 +1,7 @@
+class Refrence:
+    def __init__(self, id):
+        self.id = id
+        # This is the class data structure. create one self.variable for each data field in the refrence
+
+    def __str__(self):
+        pass
---
+++ src/repositories/refrence_repository.py
@@ -0,0 +1,18 @@
+from config import db
+from sqlalchemy import text
+
+from entities.refrence import Refrence
+
+def get_refrences():
+    """ result = db.session.execute(text("SELECT id, content, done FROM todos"))
+    todos = result.fetchall()
+    return [Todo(todo[0], todo[1], todo[2]) for todo in todos]  """
+    return None # I left the boilerplate commented for copy pasting but return a list of Refrence objects
+
+def create_refrence(content):
+    """ sql = text("INSERT INTO todos (content) VALUES (:content)")
+    db.session.execute(sql, { "content": content })
+    db.session.commit() """
+    # I left the boilerplate commented for copy pasting but create refrence inside the database from content 
+    # (containing data from the html form)
+    
