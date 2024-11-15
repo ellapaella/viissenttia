6d9b81c31efbc5aa25df1fb05176f88ce1c9b742
Juho Turunen <juho.turunen89@gmail.com>
2024-11-15 19:45:17 +0200

    Corrected names of functions

--- src/repositories/citation_repository.py
+++ src/repositories/citation_repository.py
@@ -3,16 +3,16 @@ from sqlalchemy import text
 
 from entities.citation import Article
 
-def get_refrences():
+def get_citations():
     """ result = db.session.execute(text("SELECT id, content, done FROM todos"))
     todos = result.fetchall()
     return [Todo(todo[0], todo[1], todo[2]) for todo in todos]  """
-    return None # I left the boilerplate commented for copy pasting but return a list of Refrence objects
+    return None # I left the boilerplate commented for copy pasting but return a list of citation objects
 
-def create_refrence(content):
+def create_citation(content):
     """ sql = text("INSERT INTO todos (content) VALUES (:content)")
     db.session.execute(sql, { "content": content })
     db.session.commit() """
-    # I left the boilerplate commented for copy pasting but create refrence inside the database from content 
+    # I left the boilerplate commented for copy pasting but create citation inside the database from content 
     # (containing data from the html form)
     
